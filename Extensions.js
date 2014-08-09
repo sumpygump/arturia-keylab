30f472159c9b2ab30880b008986da94f43dbd39d
Thomas Helzle <ThomasHelzle@users.noreply.github.com>
2014-08-09 19:13:40 +0200

    A wide range of script improvements...
    - Controller Display Feedback for all parameters
    - Showing the parameter even if it doesn't change (problematic due to
    fighting with the internal parameter display)
    - MidiData Function now ~12 times faster
    - Indication for Track Volume on Cursor Track.
    - Mode has two lines of text  so everything fits in
    - Drum Pads light up on notes played
    - The most important CCs are always sent to the Note Lane
    - Transport buttons light up now, although this is a bit experimental
    since it interferes with the internal
    KeyLab switching, so it has to use a timer...
    - In Arturia Mode, all CCs are also User Controls and mappable now.

--- Extensions.js
+++ Extensions.js
@@ -3,39 +3,43 @@ function MidiData(status, data1, data2) {
 	this.status = status;
 	this.data1 = data1;
 	this.data2 = data2;
+}
 
-	const noteNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+MidiData.prototype = {
+   // Note Name Array:
+	noteNames: ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
 
 	// Midi Channel:
-	this.channel = 								function(){ return (this.status & 0xF); }
+	channel: 					function() { return (this.status & 0xF); },
 
 	// Booleans for message type:
-	this.isNoteOff = 							function(){ return ((this.status & 0xF0) == 0x80) || ((this & 0xF0) == 0x90 && this.data2 == 0); }
-	this.isNoteOn = 							function(){ return (this.status & 0xF0) == 0x90; }
-	this.isKeyPressure = 					function(){ return (this.status & 0xF0) == 0xA0; }
-	this.isChannelController = 		function(){ return (this.status & 0xF0) == 0xB0; }
-	this.isProgramChange = 				function(){ return (this.status & 0xF0) == 0xC0; }
-	this.isChannelPressure = 			function(){ return (this.status & 0xF0) == 0xD0; }
-	this.isPitchBend = 						function(){ return (this.status & 0xF0) == 0xE0; }
-	this.isMTCQuarterFrame = 			function(){ return (this.status == 0xF1); }
-	this.isSongPositionPointer = 	function(){ return (this.status == 0xF2); }
-	this.isSongSelect = 					function(){ return (this.status == 0xF3); }
-	this.isTuneRequest = 					function(){ return (this.status == 0xF6); }
-	this.isTimingClock = 					function(){ return (this.status == 0xF8); }
-	this.isMIDIStart = 						function(){ return (this.status == 0xFA); }
-	this.isMIDIContinue = 				function(){ return (this.status == 0xFB); }
-	this.isMIDIStop = 						function(){ return (this.status == 0xFC); }
-	this.isActiveSensing = 				function(){ return (this.status == 0xFE); }
-	this.isSystemReset = 					function(){ return (this.status == 0xFF); }
+	isNoteOff: 					function() { return ((this.status & 0xF0) === 0x80) || ((this & 0xF0) === 0x90 && this.data2 === 0); },
+	isNoteOn: 					function() { return (this.status & 0xF0) === 0x90; },
+	isKeyPressure: 			function() { return (this.status & 0xF0) === 0xA0; },
+	isChannelController: 	function() { return (this.status & 0xF0) === 0xB0; },
+	isProgramChange: 			function() { return (this.status & 0xF0) === 0xC0; },
+	isChannelPressure: 		function() { return (this.status & 0xF0) === 0xD0; },
+	isPitchBend: 				function() { return (this.status & 0xF0) === 0xE0; },
+	isMTCQuarterFrame: 		function() { return (this.status === 0xF1); },
+	isSongPositionPointer: 	function() { return (this.status === 0xF2); },
+	isSongSelect: 				function() { return (this.status === 0xF3); },
+	isTuneRequest: 			function() { return (this.status === 0xF6); },
+	isTimingClock: 			function() { return (this.status === 0xF8); },
+	isMIDIStart: 				function() { return (this.status === 0xFA); },
+	isMIDIContinue: 			function() { return (this.status === 0xFB); },
+	isMIDIStop: 				function() { return (this.status === 0xFC); },
+	isActiveSensing: 			function() { return (this.status === 0xFE); },
+	isSystemReset: 			function() { return (this.status === 0xFF); },
 
-	this.type = function(){
+   // Message Type
+	type: function() {
 		var test = this.status & 0xF0;
 		switch (test) {
 			case 0x80:
 				return "NoteOff";
 			case 0x90:
 				// Note on with Velocity 0 is also considered Note Off:
-				if (this.data1 == 0) {
+				if (this.data1 === 0) {
 					return "NoteOff";
 				}
 				else{
@@ -53,24 +57,21 @@ function MidiData(status, data1, data2) {
 				return "PitchBend";
 		};
 		return "Other";
-	}
+	},
 
 	// For CCs when used as switches:
-	this.isOn =										function(){ return (this.data2 > 64); }
-	this.isOff =									function(){ return (this.data2 == 0); }
+	isOn:						function() { return (this.data2 > 64); },
+	isOff:					function() { return (this.data2 < 64); },
 
 	// Ranges:
-	this.data1IsInRange = 				function(low, high){ return (data1 >= low && data1 <= high); }
-	this.data2IsInRange = 				function(low, high){ return (data2 >= low && data2 <= high); }
+	data1IsInRange: 		function(low, high) { return (data1 >= low && data1 <= high); },
+	data2IsInRange: 		function(low, high) { return (data2 >= low && data2 <= high); },
 
 	// Notes:
-	this.note = function(){ return this.isNoteOn() ? noteNames[this.data1 % 12] : false; }
-	this.octave = function(){ return this.isNoteOn() ? Math.floor((this.data1 / 12)-2) : false;}
-	this.noteOctave = function(){ return this.isNoteOn() ? (this.note() + " " + this.octave()) : false; }
-	return this;
-} // End MidiData
-
-
+	note:                function() { return this.isNoteOn() ? noteNames[this.data1 % 12] : false; },
+	octave:              function() { return this.isNoteOn() ? Math.floor((this.data1 / 12)-2) : false;},
+	noteOctave:          function() { return this.isNoteOn() ? (this.note() + " " + this.octave()) : false; }
+}
 
 // A function to set the Note Table for Midi Inputs and add / subtrackt an Offset to Transpose:
 function setNoteTable(midiIn, table, offset) {
@@ -86,9 +87,9 @@ function setNoteTable(midiIn, table, offset) {
 	midiIn.setKeyTranslationTable(table);
 } // End setNoteTable.
 
-function printObject(object){
+function printObject(object) {
 	var x;
-	for(x in Object){
+	for(x in Object) {
 		println(x);
 	}
 }
